3b7e1993806eb21d7b36e4946bb5eb3008f86514
pbwauyo <pbwauyo@gmail.com>
2019-10-29 12:35:32 +0300

    edited worker profile page

--- lib/worker/Profile.dart
+++ lib/worker/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Profile extends StatefulWidget{
 
@@ -7,7 +8,137 @@ class Profile extends StatefulWidget{
 }
 
 class ProfileState extends State<Profile>{
+  final double radius = 20.0;
 
   @override
-  build(context) => Container();
+  build(context) => Container(
+    child: Column(
+      children: <Widget>[
+        Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              height: 200,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("assets/images/cleaning_mop.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 8.0, top: 15.0),
+                            child: Text("Peter Wauyo",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold
+                              ),
+                            ),
+                          ),
+                          margin: EdgeInsets.only(top: 30.0),
+                        ),
+
+                        Container(
+                          margin: EdgeInsets.only(left: 8.0),
+                          child: Text("pbwauyo@gmail.com"),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: CircleAvatar(
+                  child: Image(
+                    image: AssetImage("assets/images/default_pic.png"),
+                    fit: BoxFit.cover,
+                  ),
+                  radius: 40.0,
+                ),
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(radius)),
+                  border: Border.all(
+                    color: Colors.black26,
+                    width: 2.0,
+                  )
+                ),
+                margin: EdgeInsets.only(top: 60.0, right: 8.0),
+                child: Material(
+                  borderRadius: BorderRadius.all(Radius.circular(radius)),
+                  child: InkWell(
+                    onTap: (){
+
+                    },
+                    borderRadius: BorderRadius.all(Radius.circular(radius)),
+                    splashColor: Colors.black26,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 8.0, left: 12.0, right: 12.0, bottom: 8.0),
+                      child: Text("Edit Profile",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+
+                        ),
+
+                      ),
+                    ),
+                  ),
+                ),
+
+              ),
+            )
+          ],
+        ),
+
+        Container(
+          alignment: Alignment.centerLeft,
+          margin: EdgeInsets.only(left: 8.0),
+          child: RatingBar(
+            onRatingUpdate: (rating){
+
+            },
+            itemCount: 5,
+            itemBuilder: (context, item){
+              return Icon(Icons.star,
+                color: Colors.lightBlue,
+              );
+            },
+            glow: true,
+            glowRadius: 3.0,
+            glowColor: Colors.white,
+            initialRating: 4.5,
+            allowHalfRating: true,
+            ignoreGestures: true,
+            itemSize: 25.0,
+            unratedColor: Colors.grey,
+          ),
+        )
+      ],
+    ),
+
+  );
 }
